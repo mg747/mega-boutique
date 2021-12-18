e6da9387296d106ce34577f6d4b83788a3dc3660
mg747 <morganugiagbe77@gmail.com>
2021-12-18 15:09:56 +0000

    Fixed some error in index.html file

--- mega_boutique/settings.py
+++ mega_boutique/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-6waex_h0+lm%%&uj#)*ogxu(!dxjk&$&dpec$*_)_0emwj0e(o'
+SECRET_KEY = ','
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
