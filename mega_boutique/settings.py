ed7b760a171ff66ac66f50c843603610f32fcdd4
mg747 <morganugiagbe77@gmail.com>
2021-12-22 18:53:48 +0000

    Added sizes to product model

--- mega_boutique/settings.py
+++ mega_boutique/settings.py
@@ -147,6 +147,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
+
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
@@ -155,5 +156,3 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 FREE_DELIVERY_THRESHOLD = 50
 STANDARD_DELIVERY_PERCENTAGE = 10
-
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
