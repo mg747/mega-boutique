869d4117f3a151943cca8a045d51b4445a2b198d
mg747 <morganugiagbe77@gmail.com>
2022-01-05 10:08:01 +0000

    Fixed Allowed_Hosts link in settings.py

--- mega_boutique/settings.py
+++ mega_boutique/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = ','
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://mega-boutique.herokuapp.com/', 'localhost']
+ALLOWED_HOSTS = ['*' 'https://mega-boutique.herokuapp.com/', 'localhost']
 
 
 # Application definition
