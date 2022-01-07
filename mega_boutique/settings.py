91d214a937ac826f23aba2302090f01608364359
mg747 <morganugiagbe77@gmail.com>
2022-01-07 22:05:24 +0000

    Fixed settings error

--- mega_boutique/settings.py
+++ mega_boutique/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = ','
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['mega-boutique.herokuapp.com/', 'localhost']
+ALLOWED_HOSTS = ['mega-boutique.herokuapp.com', 'localhost']
 
 
 # Application definition
