72c39093ac4456c18e1d848dda8a0d6254421034
mg747 <morganugiagbe77@gmail.com>
2022-01-08 07:40:54 +0000

    Fixed static css error

--- mega_boutique/settings.py
+++ mega_boutique/settings.py
@@ -13,8 +13,6 @@ import django_heroku
 import boto3
 
 # Let's use Amazon S3
-s3 = boto3.resource('s3')
-
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
